b7e8521d1c786f9f0254a2c37c54aab513d13d5b
Pratik Badhe <techrescueofficial@outlook.com>
2020-01-03 18:44:49 +0530

    initial

---
+++ src/templates/post.js
@@ -0,0 +1,80 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
+
+import { Layout } from "../components/common";
+import { MetaData } from "../components/common/meta";
+
+import { Disqus } from "gatsby-plugin-disqus";
+import config from "../utils/siteConfig";
+/**
+ * Single post view (/:slug)
+ *
+ * This file renders a single post and loads all the content.
+ *
+ */
+const Post = ({ data, location }) => {
+    const post = data.ghostPost;
+    let disqusConfig = {
+        url: `${config.siteUrl + location.pathname}`,
+        identifier: post.id,
+        title: post.title
+    };
+
+    return (
+        <>
+            <MetaData data={data} location={location} type="article" />
+            <Helmet>
+                <style type="text/css">{`${post.codeinjection_styles}`}</style>
+            </Helmet>
+            <Layout>
+                <div className="container">
+                    <article className="content">
+                        {post.feature_image ? (
+                            <figure className="post-feature-image">
+                                <img
+                                    src={post.feature_image}
+                                    alt={post.title}
+                                />
+                            </figure>
+                        ) : null}
+                        <section className="post-full-content">
+                            <h1 className="content-title">{post.title}</h1>
+
+                            {/* The main post content */}
+                            <section
+                                className="content-body load-external-scripts"
+                                dangerouslySetInnerHTML={{ __html: post.html }}
+                            />
+                        </section>
+                    </article>
+
+                    <Disqus config={disqusConfig} />
+                </div>
+            </Layout>
+        </>
+    );
+};
+
+Post.propTypes = {
+    data: PropTypes.shape({
+        ghostPost: PropTypes.shape({
+            codeinjection_styles: PropTypes.object,
+            title: PropTypes.string.isRequired,
+            html: PropTypes.string.isRequired,
+            feature_image: PropTypes.string
+        }).isRequired
+    }).isRequired,
+    location: PropTypes.object.isRequired
+};
+
+export default Post;
+
+export const postQuery = graphql`
+    query($slug: String!) {
+        ghostPost(slug: { eq: $slug }) {
+            ...GhostPostFields
+        }
+    }
+`;
---
+++ src/utils/siteConfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+    siteUrl: `https://techrescue.ml`, // Site domain. Do not include a trailing slash!
+
+    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+
+    siteTitleMeta: `Tech Rescue`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Latest technology blogs and tutorials to keep you updated in this competitive and rapidly changing tech world.`, // This allows an alternative site description for meta data for pages.
+
+    shareImageWidth: 1000, // Change to the width of your default share image
+    shareImageHeight: 523, // Change to the height of your default share image
+
+    shortTitle: `Tech Rescue`, // Used for App manifest e.g. Mobile Home Screen
+    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
+    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
+    themeColor: `#15171A` // Used for Offline Manifest
+};
